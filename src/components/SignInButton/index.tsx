d08788bf4b0f6d1b63808190348a9fa3100921bf
Lucio Schenkel <lucio.schenkel@acad.pucrs.br>
2021-09-24 20:55:34 -0300

    feat: signin button layout

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,1 +1,22 @@
-//export function
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Lucio Schenkel
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in with Github
+      {/* #04d361 */}
+    </button>
+  );
+}
