49c63bea846cfbd087f8fc0d761f8e67a223b442
Lucio Schenkel <lucio.schenkel@acad.pucrs.br>
2021-09-24 23:44:09 -0300

    feat: implements github sign in

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,22 +1,31 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
+  console.log(session);
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      onClick={() => signOut()}
+      className={styles.signInButton}
+    >
       <FaGithub color="#04d361" />
-      Lucio Schenkel
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      onClick={() => signIn('github')}
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
-      {/* #04d361 */}
     </button>
   );
 }
